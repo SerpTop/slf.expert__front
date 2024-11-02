6428092d40b5be08b6bfeee82788dafcde85cf63
ADBrusov-SERPTOP <96046855+ADBrusov-SERPTOP@users.noreply.github.com>
2024-11-02 18:15:25 +0300

    add ym

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -36,6 +36,32 @@ export default defineNuxtConfig({
         { name: "msapplication-config", content: "/favicon/browserconfig.xml" },
         { name: "theme-color", content: "#ffffff" },
       ],
+
+      script: [
+        {
+          children: `
+         (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+          m[i].l=1*new Date();
+          for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+          k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+          (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+          ym(98740304, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+          });
+        `,
+        },
+      ],
+
+      noscript: [
+        {
+          children: `><div><img src="https://mc.yandex.ru/watch/98740304" style="position:absolute; left:-9999px;" alt="" /></div>`,
+          body: true,
+        },
+      ],
     },
   },
 

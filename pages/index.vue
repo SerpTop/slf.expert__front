4979d59ce13a2ba6136c5354fb293b4423db9b05
ADBrusov-SERPTOP <a.brusov@serptop.ru>
2025-10-15 19:13:00 +0300

    feat: рефакторинг компонентов и улучшение структуры кода, включая обновление стилей и добавление динамического контента в FAQ и главной странице

--- pages/index.vue
+++ pages/index.vue
@@ -95,17 +95,5 @@
           'Юридическая фирма, специализирующаяся на таможенных вопросах, арбитражных спорах и налоговом праве. Более 15 лет успешной практики.',
       },
     ],
-    script: `(function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-   m[i].l=1*new Date();
-   for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-   k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-   (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-   ym(98293834, "init", {
-        clickmap:true,
-        trackLinks:true,
-        accurateTrackBounce:true
-   });`,
-    noscript: `<div><img src="https://mc.yandex.ru/watch/98293834" style="position:absolute; left:-9999px;" alt="" /></div>`,
   });
 </script>
